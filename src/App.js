209aa389177a3cf2e36991a6d32e0f865c55a8b2
nadavschwartz <nadavschwartz58@gmail.com>
2021-01-30 09:10:16 -0800

    establish initial pages and layouts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from "react";
-import Form from './components/Form'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Form from "./components/Form";
 export default class App extends Component {
-
-
   handleChange = (e) => {
     this.setState({
-      comment: e.target.value
-    })
-  }
-  handleSubmit = (e) => {
+      comment: e.target.value,
+    });
   };
+  handleSubmit = (e) => {};
   render() {
     return (
-      <div>
-        <p>MoneyTarget</p>
-      <Form />
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Form />
+        </div>
+      </Router>
     );
   }
 }
